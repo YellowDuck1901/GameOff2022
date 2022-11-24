9a83353a2cd8587d63f4e7ecc04110094f12a7d5
YellowDuck1901 <81882696+YellowDuck1901@users.noreply.github.com>
2022-11-24 11:21:44 +0700

    update checkpoint when dead

--- Assets/Script/Platforms/Spike.cs
+++ Assets/Script/Platforms/Spike.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEditor.Sequences;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Spike : MonoBehaviour
-{   
+{
+    public LoadScene LoadLevel;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag.Equals("Player"))
         {
             StatusPlayer sp = collision.gameObject.GetComponent<StatusPlayer>();
-            sp.IsDead = true;
+            LoadLevel.openSceneWithColdDown(SceneManager.GetActiveScene().name,0f);
+
+            // reset penatly
+            PenatlyManager.Penatly = false; 
+            //sp.IsDead = true;
         }
     }
 }
